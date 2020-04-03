059fe0ac304df9a6e9eb462fc5678b5f9bbed949
Lewis <lewisbrignell@gmail.com>
2020-04-03 15:01:54 -0400

    gitignore added

--- script.js
+++ script.js
@@ -1,27 +1,8 @@
-// put shit here
 
-const getInfo = (url) => {
-    const xhr = new XMLHttpRequest();
-    //<pending>
-    // instatiate the promise
-    const myRequest = new Promise((resolve, reject) => {
-        xhr.open('GET', url);
-        xhr.onload = function () {
-            if (xhr.status === 200) {  // 200 === resolved
-                const data = JSON.parse(xhr.responseText);
-                resolve(data)
-            }
-            else {  // rejected
-                alert('Request failed.  Returned status of ' + xhr.status);
-                reject()
-            }
-        };
-        xhr.send();
-    });
-    return myRequest;
-} // get()
+document.addEventListener("DOMContentLoaded", function () {
+    // connect to moviedb api with axios
+
+    // connect to giphy api
+});
 
 // const link = `https://api.themoviedb.org/3/movie/550?api_key=aaf61b600bd7e26a96f6e5cf0dc95050&callback=test`;
-
-getInfo(link)
-   
