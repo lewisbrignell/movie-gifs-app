315e0e8ae2de2f17f0566d5ec3dfc24275986ed9
Lewis <lewisbrignell@gmail.com>
2020-04-03 15:42:25 -0400

    axios added

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", () => {
+    
     // connect to moviedb api with axios
+    axios({
+        method: 'GET',
+        url: `https://api.themoviedb.org/3/movie/550?api_key=${config.moviedbAPIKey}&callback=test`,
+        responseType: 'json',
+    })
+    .then( (res) => {
+        console.log('success: ', res);
+    })
+    .catch( (res) => {
+        console.log('oops: ', res);
+        alert('Sorry! There was an error :(')
+    });
 
     // connect to giphy api
 });
-
-const moviedbCall = `https://api.themoviedb.org/3/movie/550?api_key=aaf61b600bd7e26a96f6e5cf0dc95050&callback=test`;
